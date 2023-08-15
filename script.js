e7627deab2643e77cfac5d219ce5c2e09b302915
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2023-08-15 07:39:54 -0700

    Create controllers for game and display

--- script.js
+++ script.js
@@ -1,50 +1,175 @@
 "use strict";
+//Factory Function
+const player = (name, mark) => {
+  let score = 0;
 
-const board = document.querySelector(".game-board");
-const [...boxes] = document.querySelectorAll(".box");
+  // getters
+  const getName = () => name;
+  const getMark = () => mark;
+  const getScore = () => score;
 
-const Player = (name) => {
-  let score;
-  let mark;
-  const sayName = () => console.log(name);
+  // setters
+  const setName = (input) => (name = input);
+  const setMark = (input) => (mark = input);
+  const setScore = (input) => (score = input);
 
-  const add = () => score++;
-
-  const chooseMark = (choice) => {
-    mark = choice;
-  };
-
-  return { sayName, score, add, mark, chooseMark };
+  return { setName, getName, getMark, setMark, setScore, getScore };
 };
 
-const Game = (() => {
-  const board = ["s", "s", "s", "t", "t", "t", "r", "r", "r"];
+// Module Pattern
+const gameBoard = (() => {
+  let board = new Array(9);
 
-  const getlength = () => {
-    console.log(board.length);
+  const reset = () => {
+    for (let i = 0; i < board.length; i++) {
+      board[i] = "";
+    }
+  };
+  const setField = (index, mark) => {
+    if (!board[index]) {
+      board[index] = mark;
+      return true;
+    } else return false;
   };
 
-  const displayBoard = (array) => {
-    array.forEach((cur, i) => (cur.textContent = board[i]));
+  const getField = (index) => {
+    return board[index];
   };
 
-  const placeMarker = (element, playerMark) => {
-    element.addEventListener("click", function (e) {
-      let position = e.target.closest(".box");
-      position.textContent = playerMark;
-    });
+  const getBoard = () => {
+    return board;
   };
 
-  return { board, getlength, displayBoard, placeMarker };
+  return { reset, setField, getField, getBoard, board };
 })();
 
-let mark = "x";
+const gameController = (() => {
+  let round = 1;
+  let gameOver = false;
+  let player1 = player("john", "X");
+  let player2 = player("jacob", "O");
+  let players = [player1, player2];
+  let activePlayer = player1;
+  let results;
+  let winner;
+  let message;
 
-// board.addEventListener("click", function (e) {
-//   let position = e.target.closest(".box");
-//   console.log(e.target);
-//   console.log(this);
-//   position.textContent = mark;
-// });
+  const switchActivePlayer = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
 
-Game.placeMarker(board, mark);
+  const getActivePlayer = () => {
+    return activePlayer.getName();
+  };
+
+  const checkGameStatus = (position) => {
+    const winCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    results = winCombinations
+      .filter((combination) => combination.includes(position))
+      .some((possibleCombination) =>
+        possibleCombination.every(
+          (index) => gameBoard.getField(index) === activePlayer.getMark()
+        )
+      );
+
+    if (results) {
+      gameOver = true;
+      winner = activePlayer.getName();
+      activePlayer.setScore(activePlayer.getScore() + 1);
+    } else if (round === 9) {
+      winner = "Tie";
+      gameOver = true;
+    }
+  };
+
+  const playRound = (position) => {
+    if (!gameOver) {
+      if (gameBoard.setField(position, activePlayer.getMark())) {
+        checkGameStatus(position);
+        switchActivePlayer();
+        round++;
+      }
+    }
+    if (gameOver) {
+      message =
+        winner === "Tie"
+          ? `Game Over: It's a tie`
+          : `Game Over: ${winner} wins`;
+      displayController.displayScore();
+      displayController.displayResults(message);
+    }
+  };
+
+  const resetGame = () => {
+    round = 1;
+    gameOver = false;
+    winner = "";
+    results = "";
+    switchActivePlayer();
+    gameBoard.reset();
+  };
+
+  const resetScores = () => {
+    players.forEach((player) => player.setScore(0));
+  };
+
+  return { playRound, resetGame, resetScores, players };
+})();
+
+const displayController = (() => {
+  let gameBoardElement = document.querySelector(".game-board");
+  let newBtn = document.querySelector(".new-btn");
+  let resetBtn = document.querySelector(".reset-btn");
+  let scores = document.querySelectorAll(".score");
+  let [...boxes] = document.querySelectorAll(".box");
+  let results = document.querySelector(".results");
+
+  const displayResults = (message) => {
+    results.textContent = message;
+    results.classList.remove("hidden");
+  };
+  const displayScore = () => {
+    scores.forEach(
+      (score, index) =>
+        (score.textContent = gameController.players[index].getScore())
+    );
+  };
+
+  const displayBoard = (board) => {
+    boxes.forEach((box, index) => (box.textContent = gameBoard.board[index]));
+  };
+
+  const placeMarker = (() =>
+    gameBoardElement.addEventListener("click", function (e) {
+      let position = e.target.closest(".box");
+      let index = Number.parseInt(position.dataset.index);
+      gameController.playRound(index);
+      displayBoard();
+    }))();
+
+  const resetBoard = (() => {
+    newBtn.addEventListener("click", function () {
+      gameController.resetGame();
+      displayBoard();
+      results.classList.toggle("hidden");
+    });
+  })();
+
+  const resetScores = (() => {
+    resetBtn.addEventListener("click", function () {
+      gameController.resetScores();
+      displayScore();
+    });
+  })();
+
+  return { displayBoard, resetBoard, displayScore, displayResults };
+})();
